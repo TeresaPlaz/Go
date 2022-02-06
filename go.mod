34a9bb1499dc0a2909f75e9014a95d1dc1bf9c4f
Teresa Plaz <teresaplaz@gmail.com>
2022-02-06 17:08:47 -0500

    Mod test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TeresaPlaz/Go
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,10 @@
+package utils
+
+import "fmt"
+
+// go mod download -json | This shows a list of downloaded cached packages
+// go get github.com/Username/RepoName/Branch
+
+func HelloWorld() {
+	fmt.Println("Hello world")
+}
